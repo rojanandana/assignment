e6c3faa05ec4d73268a52cd3e6661e63648b1353
rojanandana <40118828+rojanandana@users.noreply.github.com>
2021-07-26 12:12:46 +0530

    Update reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -2,11 +2,3 @@ import React from "react";
 
 // Contexts
 export const StoreContext = React.createContext(null);
-
-// Action constants
-export const actions = {
-  GET_ITEMS: "get items",
-  GET_ITEMS_SUCCESS: "get items success",
-  ADD_ITEM: "add item",
-  REMOVE_ITEM: "remove item"
-};
